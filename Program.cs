fa6977a768305f9660e8aab46609ee1693afe0ff
Dmitriy <Dmitriy.Kh.78@yandex.ru>
2023-06-14 23:12:50 +0300

    Z3

--- Program.cs
+++ Program.cs
@@ -1,57 +1,4 @@
-﻿/*
-int Calculate(int a, int b, char sing)
-{
-    int result = 0;
-    if (sing =='+')
-    {
-        result = a +b;
-    }
-    else if (sing == '-')
-    result = a -b;
-else
-{
-Console.WriteLine("Такого знака еще нет");
-}
-return result;
-}
-*/
-/*
-int[] GetArray (int size, int minValue, int maxVale)
-{
-    int[] resultArray = new int[size];
-    for(int i = 0; i < resultArray.Length; i++)
-    {
-        resultArray[i] = new Random().Next(minValue, maxVale+1);
-    }
-    return resultArray;
-}
-int[] array = GetArray(12, -9, 9);
-Console.WriteLine($"Массив: [{String.Join("; ", array)}]");
-int posetiveSum = 0;
-int negativSum = 0;
-for (int i = 0; i < array.Length; i++)
-{
-    if (array[i] > 0)
-    {
-        posetiveSum = posetiveSum + array[i];
-    }
-    else
-    {
-        negativSum = negativSum + array[i];
-    }
-}
-Console.WriteLine($"Сумм пол {posetiveSum}, отр: {negativSum}");
-
-
-int[] InverseArray (int[] arr)
-
-    for (int= 0; int < arr.Length; i++)
-    {
-        arr[i] = arr[i] * (-1);
-    }
-    return arr;
-*/
-
+﻿
 /*
 //Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
 //Напишите программу, которая покажет количество чётных чисел в массиве.
@@ -99,3 +46,33 @@ sumEven = sumEven + array[i];
 
 Console.WriteLine($"Массив: [ {String.Join("; ", array)} ] сумму элементов, стоящих на нечётных позициях = { sumEven}");
 */
+
+
+//Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+int size =4;
+double sumEven = 0;
+
+double[] array = new double[size];
+for (int i = 0; i < size; i++)
+{
+
+    array[i] = Math.Round(new Random().NextDouble() * 100, 2);
+    
+      
+}
+double  minNumber = array[0];
+double  maxNumber = array[0];
+for (int i = 1; i < size; i++)
+if (array[i] > maxNumber)
+   {
+    maxNumber = array[i];
+   }
+else{
+    if (array[i] < minNumber )
+    minNumber = array[i];
+}
+sumEven = maxNumber - minNumber;
+sumEven = Math.Round(sumEven, 2);
+
+Console.WriteLine($"Массив:  [ {String.Join("; ", array)} ] разница между максимальным и минимальным элементом массива = [{sumEven}] ");
